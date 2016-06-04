6be42944daeb4de34442e73717a3d01ec25eeb97
Vita Pluvia <vitapluvia@gmail.com>
2016-06-04 15:29:37 -0700

    feat(remote): add simple remote example to generator

--- main.py
+++ main.py
@@ -7,28 +7,45 @@ except:
   print "pwntools must be installed"
   sys.exit(1)
 
+HOST = "google.com"
+PORT =  80
+
 FILENAME = "client.py"
+CLIENT_TYPE = "remote"
 HEADER = "#!/usr/bin/env python"
 FILE_TEMPLATE = ""
 FILE_TEMPLATE += HEADER
 FILE_TEMPLATE += """
 from pwn import *
 
-def main():
-  {}
+{}
 
+def main():
+{} 
 if __name__ == "__main__":
   main()
 """
 
+interaction = r"""
+  r.sendline('GET / HTTP/1.1\n\n')
+  print r.recvline()
+""".lstrip('\n')
+
+def chooseClientType():
+  types = ["ssh", "remote", "local", "shell"]
+  v = options("Choose a type.", types)
+  print "You chose: {}".format(types[v])
+
 def saveFile():
   client = open(FILENAME, "w")
-  contents = FILE_TEMPLATE.format("print 'Hello!'")
+  connection = "r = remote('{}', {})".format(HOST, PORT)
+  contents = FILE_TEMPLATE.format(connection, interaction)
   client.write(contents)
   client.close()
 
 def main():
   print "Running PwnUp v0.0.1"
+  chooseClientType()
   saveFile()
   print "Client Written to {}".format(FILENAME)
   print "Done."
