32b169f1097699a041f3b22003e8be2faa3ab98e
Vita Pluvia <vitapluvia@gmail.com>
2016-06-04 22:11:59 -0700

    feat(main): add cli interaction points to client output

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ except:
   print "pwntools must be installed"
   sys.exit(1)
 
-##### STUB OUT WRITE & SEND #####
+# Grab only the last 32 bytes instead of the exact match for recv's:
+LAST_BYTES = 32
+
+# - STUB OUT WRITE & SEND - #
+# io_types: { stdin: 0, stdout: 1 }
+# ALL_IO contains tuples of (io_type, value)
 OLD_WRITE = sys.stdout.write
 OLD_SEND = tube.send
+ALL_IO = []
 ALL_WRITES = []
 ALL_INPUTS = []
 
@@ -19,6 +25,7 @@ def restoreInput(): tube.send = OLD_SEND
 def stubWrite():
   def newWrite(v):
     OLD_WRITE(v)
+    ALL_IO.append((1, v))
     ALL_WRITES.append(v)
 
   sys.stdout.write = newWrite
@@ -26,6 +33,7 @@ def stubWrite():
 def stubInput():
   def newSend(tube, v):
     OLD_SEND(tube, v)
+    ALL_IO.append((0, v))
     ALL_INPUTS.append(v)
 
   tube.send = newSend
@@ -50,17 +58,12 @@ if __name__ == "__main__":
   main()
 """
 
-interaction = r"""
-  r.sendline('GET / HTTP/1.1\n\n')
-  print r.recvline()
-""".lstrip('\n')
-
 def chooseClientType():
   types = ["ssh", "remote", "local", "shell"]
   v = options("Choose a type.", types)
   log.info("You Chose: {}".format(types[v]))
 
-def saveFile():
+def saveFile(interaction):
   client = open(FILENAME, "w")
   connection = "r = remote('{}', {})".format(HOST, PORT)
   contents = FILE_TEMPLATE.format(connection, interaction)
@@ -75,20 +78,31 @@ def restoreIO():
   restoreInput()
   restoreWrite()
 
+def getIOString(tup):
+  ioType = tup[0]
+  value = tup[1]
+
+  if (ioType == 0):
+    return r"  r.send({})".format(repr(value))
+  else:
+      return r"  print r.recvuntil({})".format(repr(value[-LAST_BYTES:]))
+
 def interact():
   r = remote(HOST, PORT)
+  result = ""
   stubIO()
   r.interactive()
   restoreIO()
-  print ALL_INPUTS
-  print ALL_WRITES[1:]
+  for tup in ALL_IO[1:-2]:
+    result += "{}\n".format(getIOString(tup))
 
+  return result
 
 def main():
   log.info("Running PwnUp v0.0.1")
   chooseClientType()
-  interact()
-  saveFile()
+  interaction = interact()
+  saveFile(interaction)
   log.info("Client Written to {}".format(FILENAME))
 
 if __name__ == "__main__":
