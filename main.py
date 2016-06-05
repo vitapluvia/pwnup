28e65a0ec6843e5ce7b66f2a6377da725b820c35
Vita Pluvia <vitapluvia@gmail.com>
2016-06-05 00:09:03 -0700

    feat(main): add support for multiple connection types

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ HEADER = "#!/usr/bin/env python"
 FILE_TEMPLATE = ""
 FILE_TEMPLATE += HEADER
 FILE_TEMPLATE += """
-
 from pwn import *
+
 {}
 
 def main():
@@ -58,14 +58,30 @@ if __name__ == "__main__":
 """
 # -------------------------- #
 
-def chooseClientType():
-  types = ["ssh", "remote", "local", "shell"]
+def chooseClientType(host, port):
+  types = ["ssh", "remote", "local"]
   v = options("Choose a type.", types)
   log.info("You Chose: {}".format(types[v]))
+  if types[v] == "remote":
+    host = host or raw_input("host > ")
+    port = port or raw_input("port > ")
+    return "r = remote('{}', {})".format(host, port)
+  if types[v] == "local":
+    binary = raw_input("binary > ")
+    return "r = process('{}')".format(binary)
+  if types[v] == "ssh":
+    host = host or raw_input("host > ")
+    port = port or raw_input("port > ")
+    user = raw_input("user > ")
+    password = raw_input("password > ")
+    cmd = raw_input("cmd > ")
+    sshCmd = "sh = ssh(host='{}', user='{}', password='{}', port={})\n".format(
+        host, user, password, int(port))
+    sshCmd += "r = sh.run('{}')".format(cmd)
+    return sshCmd
 
-def saveFile(interaction, host, port):
+def saveFile(connection, interaction, host, port):
   client = open(FILENAME, "w")
-  connection = "r = remote('{}', {})".format(host, port)
   contents = FILE_TEMPLATE.format(connection, interaction)
   client.write(contents)
   client.close()
@@ -87,32 +103,37 @@ def getIOString(tup):
   else:
       return r"  print r.recvuntil({})".format(repr(value[-LAST_BYTES:]))
 
-def interact(host, port):
-  r = remote(host, port)
+def interact(connection, host, port):
+  exec(connection)
+  last = -1 if "process" not in connection else -3
   result = ""
   log.info("Press <Ctrl-D> to stop recording ...")
   stubIO()
   r.interactive()
   restoreIO()
-  for tup in ALL_IO[1:]:
+  for tup in ALL_IO[1:last]:
     result += "{}\n".format(getIOString(tup))
 
   return result
 
 def checkArgs():
-  if len(sys.argv) < 3:
-    print "Usage: pwnUp <host> <port>"
-    sys.exit(1)
-  host = sys.argv[1]
-  port = sys.argv[2]
+  host = ""
+  port = ""
+  if len(sys.argv) > 1:
+    if sys.argv[1] in ["-h", "--help"]:
+      print "Usage: pwnUp <host> <port>"
+      sys.exit(1)
+  if len(sys.argv) > 2:
+    host = sys.argv[1]
+    port = sys.argv[2]
   return host, port
 
 def main():
   host, port = checkArgs()
   log.info("Running PwnUp {}".format(VERSION))
-  # chooseClientType()
-  interaction = interact(host, port)
-  saveFile(interaction, host, port)
+  connection = chooseClientType(host, port)
+  interaction = interact(connection, host, port)
+  saveFile(connection, interaction, host, port)
   log.info("Client Written to {}".format(FILENAME))
 
 if __name__ == "__main__":
