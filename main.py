c9229429993a5228d072afbb4f8dc13a8baf4d9a
Vita Pluvia <vitapluvia@gmail.com>
2016-06-04 23:25:49 -0700

    feat(main): add arguments to control remote host & port

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ except:
   print "pwntools must be installed"
   sys.exit(1)
 
-# Grab only the last 32 bytes instead of the exact match for recv's:
+VERSION = "v0.0.1"
+
+# Grab only the last N bytes instead of the exact match for recv's:
 LAST_BYTES = 32
 
 # - STUB OUT WRITE & SEND - #
 # io_types: { stdin: 0, stdout: 1 }
 # ALL_IO contains tuples of (io_type, value)
+# -------------------------- #
 OLD_WRITE = sys.stdout.write
 OLD_SEND = tube.send
 ALL_IO = []
@@ -37,12 +40,8 @@ def stubInput():
     ALL_INPUTS.append(v)
 
   tube.send = newSend
-##############################
-
+# -------------------------- #
 FILENAME = "client.py"
-CLIENT_TYPE = "remote"
-HOST = "google.com"
-PORT =  80
 
 HEADER = "#!/usr/bin/env python"
 FILE_TEMPLATE = ""
@@ -57,15 +56,16 @@ def main():
 if __name__ == "__main__":
   main()
 """
+# -------------------------- #
 
 def chooseClientType():
   types = ["ssh", "remote", "local", "shell"]
   v = options("Choose a type.", types)
   log.info("You Chose: {}".format(types[v]))
 
-def saveFile(interaction):
+def saveFile(interaction, host, port):
   client = open(FILENAME, "w")
-  connection = "r = remote('{}', {})".format(HOST, PORT)
+  connection = "r = remote('{}', {})".format(host, port)
   contents = FILE_TEMPLATE.format(connection, interaction)
   client.write(contents)
   client.close()
@@ -87,22 +87,32 @@ def getIOString(tup):
   else:
       return r"  print r.recvuntil({})".format(repr(value[-LAST_BYTES:]))
 
-def interact():
-  r = remote(HOST, PORT)
+def interact(host, port):
+  r = remote(host, port)
   result = ""
+  log.info("Press <Ctrl-D> to stop recording ...")
   stubIO()
   r.interactive()
   restoreIO()
-  for tup in ALL_IO[1:-2]:
+  for tup in ALL_IO[1:]:
     result += "{}\n".format(getIOString(tup))
 
   return result
 
+def checkArgs():
+  if len(sys.argv) < 3:
+    print "Usage: pwnUp <host> <port>"
+    sys.exit(1)
+  host = sys.argv[1]
+  port = sys.argv[2]
+  return host, port
+
 def main():
-  log.info("Running PwnUp v0.0.1")
-  chooseClientType()
-  interaction = interact()
-  saveFile(interaction)
+  host, port = checkArgs()
+  log.info("Running PwnUp {}".format(VERSION))
+  # chooseClientType()
+  interaction = interact(host, port)
+  saveFile(interaction, host, port)
   log.info("Client Written to {}".format(FILENAME))
 
 if __name__ == "__main__":
